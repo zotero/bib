d6ef80d062605b9683f9e0ff2b7a5ac7c5439403
Tom Najdek <tom@doppnet.com>
2017-07-27 16:01:12 +0100

    Release 0.1.3

---
+++ src/js/zotero-shim/item-types.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const itemTypes = {
+	1: 'note',
+	2: 'book',
+	3: 'bookSection',
+	4: 'journalArticle',
+	5: 'magazineArticle',
+	6: 'newspaperArticle',
+	7: 'thesis',
+	8: 'letter',
+	9: 'manuscript',
+	10: 'interview',
+	11: 'film',
+	12: 'artwork',
+	13: 'webpage',
+	14: 'attachment',
+	15: 'report',
+	16: 'bill',
+	17: 'case',
+	18: 'hearing',
+	19: 'patent',
+	20: 'statute',
+	21: 'email',
+	22: 'map',
+	23: 'blogPost',
+	24: 'instantMessage',
+	25: 'forumPost',
+	26: 'audioRecording',
+	27: 'presentation',
+	28: 'videoRecording',
+	29: 'tvBroadcast',
+	30: 'radioBroadcast',
+	31: 'podcast',
+	32: 'computerProgram',
+	33: 'conferencePaper',
+	34: 'document',
+	35: 'encyclopediaArticle',
+	36: 'dictionaryEntry'
+};
+
+//reverse lookup
+Object.keys(itemTypes).map(k => itemTypes[itemTypes[k]] = k);
+module.exports = itemTypes;
---
+++ test/fixtures/zotero-item-book.js
@@ -0,0 +1,20 @@
+module.exports = {
+	'itemKey': 'PXZ5ISSF',
+	'itemVersion': 0,
+	'itemType': 'book',
+	'creators': [{
+		'firstName': 'Frank',
+		'lastName': 'Herbert',
+		'creatorType': 'author'
+	}],
+	'tags': [],
+	'title': 'Dune',
+	'ISBN': '9780450011849',
+	'date': '1 Feb. 1982',
+	'publisher': 'Hodder Paperbacks',
+	'edition': '2 edition',
+	'numPages': '608',
+	'language': 'English',
+	'place': 'London',
+	'libraryCatalog': 'Amazon'
+};
