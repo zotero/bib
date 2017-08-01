abefcceb7dc23ca38a36a0f6bd941a02aa3be2ef
Tom Najdek <tom@doppnet.com>
2017-08-01 11:19:28 +0100

    Release 0.1.7

---
+++ test/fixtures/zotero-item-paper.js
@@ -0,0 +1,60 @@
+module.exports = {
+	'itemKey': 'MTF25W26',
+	'itemVersion': 0,
+	'itemType': 'journalArticle',
+	'creators': [
+		{
+			'firstName': 'Yao',
+			'lastName': 'Zhai',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Yaoguang',
+			'lastName': 'Ma',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Sabrina N.',
+			'lastName': 'David',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Dongliang',
+			'lastName': 'Zhao',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Runnan',
+			'lastName': 'Lou',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Gang',
+			'lastName': 'Tan',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Ronggui',
+			'lastName': 'Yang',
+			'creatorType': 'author'
+		},
+		{
+			'firstName': 'Xiaobo',
+			'lastName': 'Yin',
+			'creatorType': 'author'
+		}
+	],
+	'tags': [],
+	'title': 'Scalable-manufactured randomized glass-polymer hybrid metamaterial for daytime radiative cooling',
+	'publicationTitle': 'Science',
+	'rights': 'Copyright © 2017, American Association for the Advancement of Science',
+	'date': '2017/02/09',
+	'DOI': '10.1126/science.aai7899',
+	'url': 'http://science.sciencemag.org/content/early/2017/02/08/science.aai7899',
+	'abstractNote': 'Passive radiative cooling draws heat from surfaces and radiates it into space as infrared radiation to which the atmosphere is transparent. However, the energy density mismatch between solar irradiance and the low infrared radiation flux from a near-ambient-temperature surface require materials that strongly emit thermal energy and barely absorb sunlight. We embedded resonant polar dielectric microspheres randomly in a polymeric matrix, resulting in a metamaterial that is fully transparent to the solar spectrum while having an infrared emissivity greater than 0.93 across the atmospheric window. When backed with silver coating, the metamaterial shows a noon-time radiative cooling power of 93 W/m2 under direct sunshine. More critically, we demonstrated high-throughput, economical roll-to-roll manufacturing of the metamaterial, vital for promoting radiative cooling as a viable energy technology.',
+	'language': 'en',
+	'pages': 'eaai7899',
+	'ISSN': '0036-8075, 1095-9203',
+	'libraryCatalog': 'science.sciencemag.org',
+	'accessDate': 'CURRENT_TIMESTAMP'
+};
